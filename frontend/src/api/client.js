84021fb3fa34e0178ff2d31e249db5e4c55f40ce
Varshith <varshith.kummarikunta@gmail.com>
2026-07-29 23:07:01 +0530

    Update frontend API and Socket URL for production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/api/v1`
-    : "http://localhost:4000/api/v1",
+    : "http://localhhttps://chess-game-backend-vzal.onrender.comost:4000/api/v1",
   withCredentials: true
 })
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_API_URL || "http://localhost:4000", {
+export const socket = io(import.meta.env.VITE_API_URL || "https://chess-game-backend-vzal.onrender.com", {
     withCredentials: true,
     autoConnect: false
 });
